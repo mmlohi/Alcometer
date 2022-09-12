2262abeb315f01640485370960b5e1047768ca2e
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-09-12 18:07:31 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
-function App() {
+function Alcometer() {
+  const [weight, setWeight ] = useState("")
+  const [bottles, setBottles] = useState("")
+  const [gender, setGender] = useState("male")
+  const [result, setResult] = useState(0)
+  const [time, setTime] = useState("")
+
+  function Calculate(e) {
+    e.preventDefault()
+    const grams = ((bottles * 0.33) * 8 * 4.5) - ((weight / 10) * time)
+    let result= 0
+
+    if (gender === "male") {
+      result = grams / (weight * 0.7)
+    }
+    else {
+      result = grams / (weight * 0.6)
+    }
+    if (result < 0) {
+      result = 0
+    }
+    setResult(result)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Calculating alcohol blood level</h3>
+      <form onSubmit={Calculate}>
+        <div>
+          <label>Weight </label>
+          <input name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Bottles </label>
+          <input name="bottles" type="number" step="1"value={bottles} onChange={e => setBottles(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Time </label>
+          <input name="time" type="number" step="1"value={time} onChange={e => setTime(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Gender </label>
+          <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)} /><label>Male</label>
+          <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)} /><label>Female</label>
+        </div>
+        <br></br>
+        <div>
+          <output>{result.toFixed(1)}</output>
+        </div>
+        <br></br>
+        <button>Calculate</button>
+      </form>
+    </>
   );
 }
 
-export default App;
+export default Alcometer;
+
+
+
