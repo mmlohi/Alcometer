f03cd3e154dadd190144a53723aa22413366405f
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-09-12 20:23:00 +0300

    Tehty dropdown-valikot

--- src/App.js
+++ src/App.js
@@ -34,12 +34,52 @@ function Alcometer() {
           <input name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
         </div>
         <div>
-          <label>Bottles </label>
-          <input name="bottles" type="number" step="1"value={bottles} onChange={e => setBottles(e.target.value)}></input>
+        <label>Bottles </label>
+          <select name="Bottles"value={bottles} onChange={e=> setBottles(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option></select>
         </div>
         <div>
           <label>Time </label>
-          <input name="time" type="number" step="1"value={time} onChange={e => setTime(e.target.value)}></input>
+          <select name= "Time" value={time} onChange={e=> setTime(e.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
+          <option value="16">16</option>
+          <option value="17">17</option>
+          <option value="18">18</option>
+          <option value="19">19</option>
+          <option value="20">20</option></select>
         </div>
         <div>
           <label>Gender </label>
